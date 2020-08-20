f3e54c37359f18a7c99e13f31258ea1e852f8766
Semyon Naitur <SemyonNaitur@gmail.com>
2020-08-20 22:10:00 +0300

    added params

--- http/http-server.js
+++ http/http-server.js
@@ -1,25 +1,32 @@
 'use strict'
 const http = require('http');
-const URouter = require('../utils/router');
+
+/**
+ * @typedef { import('../utils/router').URouter } URouter
+ */
 
 class HttpServer {
     /** @type {URouter} */
     _router;
+    /** @type {Object} */
+    _logger;
     /** @type {Server} */
     _server;
-    /** @type {*} */
-    _logger;
     _created = false;
     /** @type {number} */
     _listeningOn;
+    _params = {
+        defaultHeaders: {},
+    };
 
     /**
      * @param {URouter} router 
      * @param {*} [logger]
      */
-    constructor(router, logger) {
+    constructor(router, logger, params) {
         this._router = router;
         this._logger = logger;
+        this.params(params);
     }
 
     _matchUrl(url) {
@@ -34,6 +41,37 @@ class HttpServer {
         }
     }
 
+    _objMerge(...obj) {
+        return Object.assign({}, ...obj);
+    }
+
+    /**
+     * @param {Object} [params] 
+     * @return {Object | void}
+     */
+    params(params) {
+        if (!params) {
+            return this._objMerge({}, this._params);
+        }
+        if (typeof params !== 'object') throw new TypeError(`Invalid params object ${params}`);
+        this._params = this._objMerge(this._params, this._objMerge({}, params));
+    }
+
+    /**
+     * @param {string} property 
+     * @param {*} value 
+     * @return {*}
+     */
+    param(property, value) {
+        if (typeof value === 'undefined') {
+            const val = this._params[property];
+            return (val && (typeof val === 'object')) ? this._objMerge({}, val) : val;
+        }
+        const update = {};
+        update[property] = this._objMerge({}, value);
+        this._params = this._objMerge(this._params, update);
+    }
+
     /**
      * @return {HttpServer}
      */
@@ -42,13 +80,13 @@ class HttpServer {
         const server = http.createServer((req, res) => {
             this._log('--- Request start ---');
             this._log(req.url);
-            const matchedRoute = this._matchUrl(req.url);
+            const matched = this._matchUrl(req.url);
 
-            if (!matchedRoute) {
+            if (!matched) {
                 res.writeHead(404);
                 res.end('Not Found.');
             } else {
-                matchedRoute.func(req, res, matchedRoute.params);
+                matched.route.func(req, res, matched.params);
             }
             this._log('--- Request end ---');
         });
@@ -81,9 +119,24 @@ class HttpServer {
      * @param {ServerResponse} res 
      * @param {string} data 
      * @param {number} [code=200] 
+     * @param {Object} [headers={}]
      */
-    writeHtml(res, data, code = 200) {
-        res.writeHead(code, { 'Content-Type': 'text/html', 'Cache-Control': 'no-cache, no-store, must-revalidate' });
+    writeHtml(res, data, code = 200, headers = {}) {
+        headers = this._objMerge(this.param('defaultHeaders'), headers, { 'Content-Type': 'text/html' });
+        res.writeHead(code, headers);
+        res.write(data);
+        res.end();
+    }
+
+    /**
+     * @param {ServerResponse} res 
+     * @param {string} data 
+     * @param {number} [code=200] 
+     * @param {Object} [headers={}]
+     */
+    writeJson(res, data, code = 200, headers = {}) {
+        headers = this._objMerge(this.param('defaultHeaders'), headers, { 'Content-Type': 'application/json' });
+        res.writeHead(code, headers);
         res.write(data);
         res.end();
     }
