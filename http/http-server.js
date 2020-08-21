4e3354959a10232acddfcf32f72bb62c7360e846
Semyon Naitur <SemyonNaitur@gmail.com>
2020-08-21 19:17:38 +0300

    refactored router

--- http/http-server.js
+++ http/http-server.js
@@ -21,7 +21,7 @@ class HttpServer {
 
     /**
      * @param {URouter} router 
-     * @param {*} [logger]
+     * @param {Object} [logger]
      */
     constructor(router, logger, params) {
         this._router = router;
@@ -137,7 +137,7 @@ class HttpServer {
     writeJson(res, data, code = 200, headers = {}) {
         headers = this._objMerge(this.param('defaultHeaders'), headers, { 'Content-Type': 'application/json' });
         res.writeHead(code, headers);
-        res.write(data);
+        res.write(JSON.stringify(data));
         res.end();
     }
 
